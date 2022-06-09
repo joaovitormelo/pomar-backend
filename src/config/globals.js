23bae5c06b58dee73dc91908d0d2cec94c30659b
joaovitormelo <49257395+joaovitormelo@users.noreply.github.com>
2022-06-09 15:39:50 -0300

    Update 2

--- src/config/globals.js
+++ src/config/globals.js
@@ -1,13 +1,6 @@
 //LOCAL
 const corsAllow = ['http://127.0.0.1:8080', 'http://localhost:8080'];
 const port = 8080;
-const dbConfig = {
-    host: "sql209.epizy.com",
-    user: "epiz_31881207",
-    password: "ZrLTEISMKQeXbcL",
-    database: "epiz_31881207_pomar",
-    multipleStatements: true
-};
 
 module.exports = {
     dbConfig,
