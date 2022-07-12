abc91717fe88cdb9111fab00e216a1ce2ace90b6
joaovitormelo <49257395+joaovitormelo@users.noreply.github.com>
2022-07-12 18:41:28 -0300

    Failing Test

--- __tests__/unit/pessoa.test.js
+++ __tests__/unit/pessoa.test.js
@@ -1,6 +1,6 @@
 describe("Pessoa CRUD Test", () => {
   it("should print Hi", () => {
     const a = "Calaboca já morreu";
-    expect(a).toMatch(/calaboca/gi);
+    expect(a).toMatch(/calaa/gi);
   });
 });
