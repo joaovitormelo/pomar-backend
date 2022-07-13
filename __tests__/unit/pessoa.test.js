f4db75eb841bfa5f7647ddf776a03e1f4e842c58
joaovitormelo <49257395+joaovitormelo@users.noreply.github.com>
2022-07-13 19:28:00 -0300

    c

--- __tests__/unit/pessoa.test.js
+++ __tests__/unit/pessoa.test.js
@@ -1,6 +1,6 @@
 describe("Pessoa CRUD Test", () => {
   it("should print Hi", () => {
-    const a = "Caabaaavaaa ba";
+    const a = "Caasbaaavaaa ba";
     expect(a).toMatch(/calaa/gi);
   });
 });
