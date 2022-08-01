2debb2f46a0ffe844dfdece2a45cd9ef804b6f81
joaovitormelo <49257395+joaovitormelo@users.noreply.github.com>
2022-08-01 07:57:09 -0300

    Updated database variables (from env)

--- src/config/globals.js
+++ src/config/globals.js
@@ -1,19 +1,17 @@
 //LOCAL
 const corsAllow = ["http://127.0.0.1:8080", "http://localhost:8080"];
-const port = 8080 || process.env.PORT;
-const herokuToken = "fcfb1a54-486e-42a1-a58a-666c2d765c39";
+const port = process.env.PORT;
 
 const databaseConfig = {
-  user: "dhnmvthb",
-  host: "tuffi.db.elephantsql.com",
-  database: "dhnmvthb",
-  password: "NG5_XEzNsC0UWiQ0iwrhJo2HAqofsIeK",
-  port: "5432",
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
 };
 
 module.exports = {
   port,
   corsAllow,
-  herokuToken,
   databaseConfig,
 };
