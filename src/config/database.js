0a2f77b6a10dd8d70a1205d76fc7183a98385ca8
joaovitormelo <49257395+joaovitormelo@users.noreply.github.com>
2022-08-01 07:47:51 -0300

    Started to load database variables from process.env

--- src/config/database.js
+++ src/config/database.js
@@ -8,25 +8,6 @@ var connection;
 
 module.exports = () => {
   connection = new Client(databaseConfig);
-  /*const response = await axios
-    .get("https://api.heroku.com/apps/pomar-database/config-vars", {
-      headers: {
-        Authorization: "Bearer " + herokuToken,
-        Accept: "application/vnd.heroku+json; version=3",
-      },
-    })
-    .then(
-      (data) =>
-        (connection = new Client({
-          connectionString: data.data["DATABASE_URL"],
-          ssl: {
-            rejectUnauthorized: false,
-          },
-        }))
-    )
-    .catch((e) => {
-      console.log(e.response);
-    });*/
   connection.connect();
   console.log("DATABASE connected");
   return connection;
