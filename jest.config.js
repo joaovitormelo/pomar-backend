afe04ef0d4efa1d6d8ca9c02d9c37daba22d4b93
joaovitormelo <49257395+joaovitormelo@users.noreply.github.com>
2022-08-11 18:49:46 -0300

    Removed Sequelize stuff

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   bail: true,
-  automock: true,
   testEnvironment: "node",
   transform: {
     "^.+\\.(t|j)sx?$": "@swc/jest",
